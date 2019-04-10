5da0481e2f3d3e2dbe1cb3942e1dd93d8a66d031
ldy121 <dy121.lee@gmail.com>
2019-04-10 22:02:19 +0900

    make generate.sh to high coverage script

--- inception_v3/generate.sh
+++ inception_v3/generate.sh
@@ -4,58 +4,12 @@ python_script=cnn_neural_network.py
 input_path=/home/dy121/tensorflow/pretrained_inception_v3/inception_v3.ckpt
 input_name=input
 output_name=InceptionV3/Predictions/Reshape_1
-
-pre_process() {
-	rm -rf ${name}
-	mkdir ${name}
-}
-
-post_process() {
-	/opt/script/optimize_inference.sh \
-		model.pb \
-		${input_name} \
-		${output_name}
-
-	mv model.pb ${name}/${name}.pb 
-	mv opt_model.pb ${name}/opt_${name}.pb 
-}
-
-inception_v3_freeze() {
-	/opt/script/freeze_graph.sh \
-		${name}/${name}.pbtxt \
-		${name}/${name} \
-		${output_name}
-}
-
-prunable_inception_v3_freeze() {
-	/opt/script/freeze_pruning_graph.sh \
-		${name}/ \
-		${output_name}
-}
-
-generate_pretrained_inception_v3() {
-	python3 ${python_script} ${name} ${input_path} ${name}/${name}
-	inception_v3_freeze
-}
-
-generate_pretrained_prunable_inception_v3() {
-	python3 ${python_script} ${name} ${input_path} ${name}/${name}
-	prunable_inception_v3_freeze
-}
-
-generate_prunable_inception_v3() {
-	python3 ${python_script} ${name} ${name}/${name}
-	prunable_inception_v3_freeze
-}
-
-generate_zero_inception_v3() {
-	python3 ${python_script} ${name} ${name}/${name}
-	inception_v3_freeze
-}
-
+inception_v3_script=/opt/script/inception_v3.sh
 
 if [ $# -eq 1 ];
 then
+	. ${inception_v3_script}
+
 	name=$1
 	pre_process
 	generate_${name}
